382f2c4314e30c671b2cb9274c531f60aebc0ef7
SUBHAM BANERJEE <subhambanerjee2003@gmail.com>
2023-10-18 12:01:19 +0530

    added some new components to website

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def homePage():
     return render_template('index.html')
+
+@app.route("/about")
+def aboutPage():
+    return render_template('about.html');
+
+@app.route("/login")
+def loginPage():
+    return render_template('login.html');
+
+@app.route("/register")
+def registerPage():
+    return render_template('register.html');
 
 if __name__ == '__main__':
     app.run(debug=True)
+
